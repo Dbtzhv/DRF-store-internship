ddd83692ab63c05027e09c5c1abbaefb17203f06
Deni <denibatyzhev@gmail.com>
2023-03-09 23:58:13 +0500

    base64(put method) commit

--- products/serializers.py
+++ products/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from .models import ProductModel, PictureModel
 from drf_extra_fields.fields import Base64ImageField
+from drf_writable_nested import WritableNestedModelSerializer
 
 
 class PictureSerializer(serializers.ModelSerializer):
@@ -11,7 +12,7 @@ class PictureSerializer(serializers.ModelSerializer):
         exclude = ('product',)
 
 
-class ProductSerializer(serializers.ModelSerializer):
+class ProductSerializer(WritableNestedModelSerializer, serializers.ModelSerializer):
     images = PictureSerializer(many=True, required=True)
     # category = serializers.SlugRelatedField(slug_field='name', read_only=True)
 
@@ -20,9 +21,9 @@ class ProductSerializer(serializers.ModelSerializer):
         fields = ['id', 'title', 'category', 'description',
                   'price', 'general_quantity', 'images', 'characteristics']
 
-    def create(self, validated_data):
-        choice_validated_data = validated_data.pop('images')
-        product = ProductModel.objects.create(**validated_data)
-        for i in choice_validated_data:
-            PictureModel.objects.create(product=product, **i)
-        return product
+    # def create(self, validated_data):
+    #     choice_validated_data = validated_data.pop('images')
+    #     product = ProductModel.objects.create(**validated_data)
+    #     for i in choice_validated_data:
+    #         PictureModel.objects.create(product=product, **i)
+    #     return product
